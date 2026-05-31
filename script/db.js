b86fe4908a93672a3e0ce98e4cbb7859dabf1ccb
DanielArg100 <danicaffe100@gmail.com>
2026-05-31 17:48:28 -0300

    FinalizadoProductosYContactos(PorFin)

--- script/db.js
+++ script/db.js
@@ -1,21 +1,49 @@
 export const ListaProductos = [{
-    nombre: "PlaceHolder1",
-    precio: "PlaceHolder2",
-    stock: "PlaceHolder3",
-    img: ""
+    id: "fk",
+    nombre: "Funko Pop Jack The Ripper",
+    precio: 1655,
+    stock: 1,
+    img: "../src/Productos/Producto2FK.png"
 },{
-    nombre: "PlaceHolder1",
-    precio: "PlaceHolder2",
-    stock: "PlaceHolder3",
-    img: ""
+    id: "ropa",
+    nombre: "Remera Jack The Ripper",
+    precio: 2000,
+    stock: 9,
+    img: "../src/Productos/Producto3Ropa.png"
 },{
-    nombre: "PlaceHolder1",
-    precio: "PlaceHolder2",
-    stock: "PlaceHolder3",
-    img: ""
+    id: "ropa",
+    nombre: "Remera Sekke Bronzazza",
+    precio: 1940,
+    stock: 15,
+    img: "../src/Productos/Producto4Ropa.png"
 },{
-    nombre: "PlaceHolder1",
-    precio: "PlaceHolder2",
-    stock: "PlaceHolder3",
-    img: ""
+    id: "extra",
+    nombre: "Requiem",
+    precio: 2165,
+    stock: 2,
+    img: "../src/Productos/Producto6Extra.png"
+},{
+    id: "extra",
+    nombre: "???",
+    precio: 5350,
+    stock: 1,
+    img: "../src/Productos/Producto7Extra.png"
+},{
+    id: "fk",
+    nombre: "Funko Pop Sekke Bronzazza",
+    precio: 3025,
+    stock: 2,
+    img: "../src/Productos/Producto1FK.png"
+},{
+    id: "ropa",
+    nombre: "Set Jack The Ripper",
+    precio: 9015,
+    stock: 7,
+    img: "../src/Productos/Producto5Ropa.png"
+},{
+    id: "extra",
+    nombre: "Peluche Mantis Verde",
+    precio: 105,
+    stock: 40,
+    img: "../src/Productos/Producto8Extra.png"
 }]
--- script/main.js
+++ script/main.js
@@ -4,40 +4,47 @@ import { ListaProductos } from "./db.js";
 
 function MostrarProductos(lista) {
     let contenedor = document.querySelector(".contenedor")
-    contenedor.innerHTML = ""
-    lista.forEach(i => {
-        let nuevoDiv = document.createElement("div")
-        nuevoDiv.classList.add("Tarjeta")
-        nuevoDiv.innerHTML = `
-        <div>
-            <h1>${i.nombre}</h1>
+    if (contenedor) {
+        contenedor.innerHTML = ""
+        lista.forEach(i => {
+            let nuevoDiv = document.createElement("div")
+            nuevoDiv.classList.add("Tarjeta")
+
+            nuevoDiv.innerHTML = `
+
             <img src="${i.img}">
-        </div>
-        <div>
-            <p>${i.precio}</p>
-            ${i.Stock < 3 ? '<span style=color:red>Stock Bajo</span>' : ""}
-            <button>Agregar Al Carrito</button>
-            <button>♥️</button>
-        </div>
+
+            <h1>${i.nombre}</h1>
+
+            <p>$${i.precio}</p>
+
+            ${i.stock < 3 ? '<span style="color: red;">Stock Bajo</span>' : '<span style="color:#124527;">Stock Alto</span>'}
+
+            <div class="Tarjeta-footer">
+                <button type="button" class="Agregar-Carro" onclick="MostrarCarro()">Agregar Al Carro</button>
+                <button class="CoranzonDeLosHuevos" onclick="MostrarCorazon()">♥️</button>
+            </div>
         `
-        contenedor.appendChild(nuevoDiv)
-    });
+            contenedor.appendChild(nuevoDiv)
+        });
+    }
 }
-/*
+
 MostrarProductos(ListaProductos)
 
 let texto = document.querySelector(".texto")
 let contenedor = document.querySelector(".contenedor")
 
-texto.addEventListener("keyup", function () {
-    let filtrar = ListaProductos.filter(i=>i.nombre.toLowerCase().includes(texto.value.toLowerCase().trim()))
-    if (filtrar.length>0) {
-        MostrarProductos(filtrar)
-    } else {
-        contenedor.innerHTML=`<p style="color: red;">No Se Encontraron Coincidencias...</p>`
-    }
-})
-*/
+if (texto) {
+    texto.addEventListener("keyup", function () {
+        let filtrar = ListaProductos.filter(i => i.nombre.toLowerCase().includes(texto.value.toLowerCase().trim()))
+        if (filtrar.length > 0) {
+            MostrarProductos(filtrar)
+        } else {
+            contenedor.innerHTML = `<p style="color: red;">No Se Encontraron Coincidencias...</p>`
+        }
+    })
+}
 
 const BMO = document.querySelector(".mOscuro")
 const body = document.getElementById("cuerpo")
@@ -47,7 +54,12 @@ const pieDePágina = document.getElementById("Pie")
 const fooha = document.getElementById("fooha")
 const foohatext = document.getElementById("foohatext")
 
+const sistemaFiltradoO = document.getElementById("sistemaFiltradoO")
+
+const TarjetaO = document.querySelector(".contenedor")
+
 let estado = "Oscuro"
+
 
 
 BMO.innerHTML = `
@@ -77,6 +89,70 @@ window.CambiarModo = function () {
     }
     if (foohatext) {
         foohatext.classList.toggle("foohatextO")
-    }    
+    }
+    if (sistemaFiltradoO) {
+        sistemaFiltradoO.classList.toggle("sistemaFiltradoO")
+    }
+    if (TarjetaO) {
+        for (let i = 0; i < TarjetaO.childElementCount; i++) {
+            TarjetaO.children[i].classList.toggle("TarjetaO")
+            console.log(TarjetaO.children[i])
+            console.log(TarjetaO.children[i].classList)
+        }
+    }
 }
 
+let X = document.getElementById("X")
+let R = document.getElementById("R")
+let F = document.getElementById("F")
+let E = document.getElementById("E")
+
+X.addEventListener("click", function () {
+    MostrarProductos(ListaProductos)
+})
+R.addEventListener("click", function () {
+    let filtrar = ListaProductos.filter(i => i.id.includes("ropa"))
+    MostrarProductos(filtrar)
+})
+F.addEventListener("click", function () {
+    let filtrar = ListaProductos.filter(i => i.id.includes("fk"))
+    MostrarProductos(filtrar)
+})
+E.addEventListener("click", function () {
+    let filtrar = ListaProductos.filter(i => i.id.includes("extra"))
+    MostrarProductos(filtrar)
+})
+
+
+window.MostrarCarro = function () {
+    let timerInterval;
+    Swal.fire({
+        title: "Añadido Al Carrito",
+        timer: 2000,
+        timerProgressBar: true,
+        didOpen: () => {
+            Swal.showLoading();
+            const timer = Swal.getPopup().querySelector("b");
+            timerInterval = setInterval(() => {
+                timer.textContent = `${Swal.getTimerLeft()}`;
+            }, 100);
+        },
+        willClose: () => {
+            clearInterval(timerInterval);
+        }
+    }).then((result) => {
+        /* Read more about handling dismissals below */
+        if (result.dismiss === Swal.DismissReason.timer) console.log("I was closed by the timer");
+    });
+}
+
+window.MostrarCorazon = function () {
+    Swal.fire({
+        title: "💖",
+        text: "Añadido A Favoritos",
+        imageUrl: "../src/srcExtra/Gatos/elgatitoloves-elgatitolover.gif",
+        imageWidth: 200,
+        imageHeight: 200,
+        imageAlt: "Custom image"
+    });
+}
